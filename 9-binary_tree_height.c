20c641151013d172c7750115a227c829a1652f57
Abdul-Qudus <akindelequdus2@gmail.com>
2024-06-06 14:49:02 -0700

    New

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-	if (!tree)
-		return (0);
-
-	size_t left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	size_t right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return ((left_height > right_height) ? left_height : right_height);
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
