f73bce32129e439754027e593a6ac2b5945a8d30
Abdul-Qudus <akindelequdus2@gmail.com>
2024-06-06 15:13:59 -0700

    New

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes_c = 0;
+	size_t nodes = 0;
 
 	if (tree)
 	{
-		nodes_c += (tree->left || tree->right) ? 1 : 0;
-		nodes_c += binary_tree_nodes(tree->left);
-		nodes_c += binary_tree_nodes(tree->right);
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
 	}
 	return (nodes);
 }
